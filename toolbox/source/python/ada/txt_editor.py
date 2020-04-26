0aeea43202172d4f017095fc2c611802063b89a0
Yuquan Shan <springshan13@gmail.com>
2020-04-25 21:47:02 -0700

    [ada] make vim-like txt editor functionally ready

--- toolbox/source/python/ada/txt_editor.py
+++ toolbox/source/python/ada/txt_editor.py
@@ -1,3 +1,4 @@
+import copy
 import curses
 import curses.textpad
 
@@ -6,7 +7,10 @@ VIEW_STR = "[VIEW MODE]"
 CMD_STR = "[CMD MODE]"
 INSERT_STR = "[INSERT MODE]"
 UNKNOWN_STR = "[UNKNOW STATE!!!]"
-#
+
+MOVE_KEYS = [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT]
+BORDER_LINES = 2
+
 # class Textbox(curses.textpad.Textbox):
 #     def do_command(self, ch):
 #         if ch == 127:
@@ -15,13 +19,26 @@ UNKNOWN_STR = "[UNKNOW STATE!!!]"
 #             super(curses.textpad.Textbox, self).do_command(ch)
 
 
+# heler function to get the code of key
+def keyLookup():
+    stdscr = curses.initscr()
+    stdscr.keypad(True)
+    ch = stdscr.getkey()
+    curses.endwin()
+    print(ch)
+
+
 class TxtEditor:
     def __init__(self, content):
         self.content_ = content
+        if len(self.content_) == 0:
+            self.content_.append('')
         ## 0 is view mode, 1 is cmd mode, 2 is insert mode
         self.state_ = 0
         self.cmd_buff_ = ""
-        self.stay_editing = True
+        self.stay_editing_ = True
+        # cursor pos
+        self.yx_ = [BORDER_LINES, 0]
 
 
     def _render_state(self, window):
@@ -37,51 +54,97 @@ class TxtEditor:
 
 
     def _render_content(self, window):
-        window.addstr(0, 0, '\n'.join(self.content_))
+        yx = curses.getsyx()
+        window.addstr(BORDER_LINES, 0, '\n'.join(self.content_))
 
 
     def _take_and_process_view(self, window):
         c = window.getch()
         if c == ord('i'):
             self.state_ = 2
+            self.yx_ = [BORDER_LINES, 0]
         elif c == ord(':'):
             self.state_ = 1
             self.cmd_buff_ = ":"
+            self.yx_ = [BORDER_LINES - 1, len(self.cmd_buff_)]
+        elif c in MOVE_KEYS:
+            self._move_cursor(c)
 
 
     def _take_and_process_cmd(self, window):
+        # window.move(BORDER_LINES - 1, len(self.cmd_buff_))
         c = window.getch()
         if c >= 97 and c <= 122:
             self.cmd_buff_ = self.cmd_buff_ + chr(c)
-        elif c == 10:
+            self.yx_ = [BORDER_LINES - 1, len(self.cmd_buff_)]
+        elif c == 10: # ENTER
             if self.cmd_buff_ == ":q":
-                self.stay_editing = False
-        window.move(2, len(self.cmd_buff_))
+                self.stay_editing_ = False
+                self.cmd_buff_ = ""
+        elif c == 27: # ESC
+            window.clear()
+            self._reset_state()
 
 
     def _take_and_process_insert(self, window):
         c = window.getch()
-        self.stay_editing = True
-        pass
+        lc = copy.copy(self.yx_)
+        lc[0] = lc[0] - BORDER_LINES
+        if c == 27: # ESC
+            window.clear()
+            self._reset_state()
+        elif c in MOVE_KEYS:
+            self._move_cursor(c)
+        elif c == 127:
+            # we delete char with idx lc[1] - 1 of the line
+            if lc[1] - 1 >= 0:
+                self.content_[lc[0]] = self.content_[lc[0]][:lc[1] - 1] + self.content_[lc[0]][lc[1]:]
+            self._move_cursor(curses.KEY_LEFT)
+        elif c >= 32 and c <= 126:
+            self.content_[lc[0]] = self.content_[lc[0]][:lc[1]] + chr(c) + self.content_[lc[0]][lc[1]:]
+            self._move_cursor(curses.KEY_RIGHT)
+
+
+    def _reset_state(self):
+        self.state_ = 0
+        self.cmd_buff_ = ""
+        self.stay_editing_ = True
+        self.yx_ = [BORDER_LINES, 0]
+
+
+    def _move_cursor(self, move):
+        assert move in MOVE_KEYS, "UNKNOWN MOVE KEYS"
+        if move == MOVE_KEYS[0]:
+            self.yx_[0] = max(BORDER_LINES, self.yx_[0] - 1)
+            self.yx_[1] = min(len(self.content_[self.yx_[0] - BORDER_LINES]), self.yx_[1])
+        elif move == MOVE_KEYS[1]:
+            self.yx_[0] = min(BORDER_LINES + len(self.content_) - 1,  self.yx_[0] + 1)
+            self.yx_[1] = min(len(self.content_[self.yx_[0] - BORDER_LINES]), self.yx_[1])
+        elif move == MOVE_KEYS[2]:
+            self.yx_[1] = max(0, self.yx_[1] - 1)
+        elif move == MOVE_KEYS[3]:
+            self.yx_[1] = min(len(self.content_[self.yx_[0] - BORDER_LINES]), self.yx_[1] + 1)
 
 
     def run(self):
-        stdscr = curses.initscr()
-        txtscr = stdscr.subwin(2, 0)
-        txtscr.keypad(True)
-        self.stay_editing = True
-        self._render_state(stdscr)
-        self._render_content(txtscr)
-        stdscr.move(1, 0)
-        txtscr.move(0, 0)
-        while self.stay_editing:
+        self._reset_state()
+        scr = curses.initscr()
+        curses.noecho()
+        scr.keypad(True)
+        scr.clearok(True)
+        self._render_state(scr)
+        self._render_content(scr)
+        while self.stay_editing_:
+            scr.move(*self.yx_)
             if self.state_ == 0:
-                self._take_and_process_view(txtscr)
+                self._take_and_process_view(scr)
             elif self.state_ == 1:
-                self._take_and_process_cmd(stdscr)
+                self._take_and_process_cmd(scr)
             elif self.state_ == 2:
-                self._take_and_process_insert(txtscr)
-            self._render_state(stdscr)
-            self._render_content(txtscr)
+                self._take_and_process_insert(scr)
+            self._render_state(scr)
+            self._render_content(scr)
+        scr.clear()
         curses.endwin()
+        self._reset_state()
         return self.content_
