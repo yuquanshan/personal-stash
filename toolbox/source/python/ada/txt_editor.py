3a8a59e33f225fab13519af963716d63c3d90876
Yuquan Shan <springshan13@gmail.com>
2020-04-26 00:01:28 -0700

    [ada] add ineditable area in txt editor

--- toolbox/source/python/ada/txt_editor.py
+++ toolbox/source/python/ada/txt_editor.py
@@ -13,16 +13,21 @@ BORDER_LINES = 2
 
 
 class TxtEditor:
-    def __init__(self, content):
+    def __init__(self, content, ineditable_content = None):
         self.content_ = content
         if len(self.content_) == 0:
             self.content_.append('')
+        if not ineditable_content or len(content) == 0:
+            self.ineditable_content_ = ['' for x in self.content_]
+        else:
+            assert len(content) == len(ineditable_content)
+            self.ineditable_content_ = ineditable_content
         ## 0 is view mode, 1 is cmd mode, 2 is insert mode
         self.state_ = 0
         self.cmd_buff_ = ""
         self.stay_editing_ = True
         # cursor pos
-        self.yx_ = [BORDER_LINES, 0]
+        self.yx_ = [BORDER_LINES, len(self.ineditable_content_[0])]
 
 
     def _render_state(self, window):
@@ -39,7 +44,7 @@ class TxtEditor:
 
     def _render_content(self, window):
         yx = curses.getsyx()
-        window.addstr(BORDER_LINES, 0, '\n'.join(self.content_))
+        window.addstr(BORDER_LINES, 0, '\n'.join([x[0] + x[1] for x in zip(self.ineditable_content_, self.content_)]))
 
 
     def _take_and_process_view(self, window):
@@ -76,6 +81,7 @@ class TxtEditor:
         c = window.getch()
         lc = copy.copy(self.yx_)
         lc[0] = lc[0] - BORDER_LINES
+        lc[1] = lc[1] - len(self.ineditable_content_[lc[0]])
         if c == 27: # ESC
             window.clear()
             self._reset_state(False)
@@ -97,21 +103,21 @@ class TxtEditor:
         self.cmd_buff_ = ""
         self.stay_editing_ = True
         if reset_cursor:
-            self.yx_ = [BORDER_LINES, 0]
+            self.yx_ = [BORDER_LINES, len(self.ineditable_content_[0])]
 
 
     def _move_cursor(self, move):
         assert move in MOVE_KEYS, "UNKNOWN MOVE KEYS"
         if move == MOVE_KEYS[0]:
             self.yx_[0] = max(BORDER_LINES, self.yx_[0] - 1)
-            self.yx_[1] = min(len(self.content_[self.yx_[0] - BORDER_LINES]), self.yx_[1])
+            self.yx_[1] = max(len(self.ineditable_content_[self.yx_[0] - BORDER_LINES]), min(len(self.content_[self.yx_[0] - BORDER_LINES]) + len(self.ineditable_content_[self.yx_[0] - BORDER_LINES]), self.yx_[1]))
         elif move == MOVE_KEYS[1]:
             self.yx_[0] = min(BORDER_LINES + len(self.content_) - 1,  self.yx_[0] + 1)
-            self.yx_[1] = min(len(self.content_[self.yx_[0] - BORDER_LINES]), self.yx_[1])
+            self.yx_[1] = max(len(self.ineditable_content_[self.yx_[0] - BORDER_LINES]), min(len(self.ineditable_content_[self.yx_[0] - BORDER_LINES]) + len(self.content_[self.yx_[0] - BORDER_LINES]), self.yx_[1]))
         elif move == MOVE_KEYS[2]:
-            self.yx_[1] = max(0, self.yx_[1] - 1)
+            self.yx_[1] = max(len(self.ineditable_content_[self.yx_[0] - BORDER_LINES]), self.yx_[1] - 1)
         elif move == MOVE_KEYS[3]:
-            self.yx_[1] = min(len(self.content_[self.yx_[0] - BORDER_LINES]), self.yx_[1] + 1)
+            self.yx_[1] = min(len(self.ineditable_content_[self.yx_[0] - BORDER_LINES]) + len(self.content_[self.yx_[0] - BORDER_LINES]), self.yx_[1] + 1)
 
 
     def run(self):
